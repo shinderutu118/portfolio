a390666c5066f6f2a57457ca28065cdd3e326d98
rutuja-ri <rutuja.s@riequation.com>
2025-10-03 16:58:26 +0530

    added route

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
   plugins: [react()],
 })
+
+
+// export default defineConfig({
+//   base: "/portfolio/",
+//   plugins: [react()],
+// })
